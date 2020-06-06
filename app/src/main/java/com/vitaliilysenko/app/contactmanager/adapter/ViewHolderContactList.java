d12a0ce5c0b3731231b666be5862dc0747682c4c
Vitalii <vit47903@gmail.com>
2020-06-07 00:00:00 +0300

    changed item_contact.xml

--- app/src/main/java/com/vitaliilysenko/app/contactmanager/adapter/ViewHolderContactList.java
+++ app/src/main/java/com/vitaliilysenko/app/contactmanager/adapter/ViewHolderContactList.java
@@ -1,13 +1,13 @@
 package com.vitaliilysenko.app.contactmanager.adapter;
 
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.squareup.picasso.Picasso;
 import com.vitaliilysenko.app.contactmanager.R;
 import com.vitaliilysenko.app.contactmanager.data.Contact;
@@ -18,7 +18,7 @@ public class ViewHolderContactList extends RecyclerView.ViewHolder {
     private TextView lastName;
     private TextView surName;
     private TextView numberPhone;
-    private FloatingActionButton deleteContact;
+    private Button deleteContact;
     private AdapterContactList.OnContactListClickListener onContactClickListener;
     
     public ViewHolderContactList(@NonNull View itemView, AdapterContactList
@@ -44,16 +44,16 @@ public class ViewHolderContactList extends RecyclerView.ViewHolder {
         deleteContact.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                    onContactClickListener
-                            .onContactDelete(getAdapterPosition());
-        
+                onContactClickListener
+                        .onContactDelete(getAdapterPosition());
+                
             }
         });
         
         lastName.setText(contact.getName());
         surName.setText(contact.getSurname());
         numberPhone.setText(contact.getEmail());
-    
+        
         Picasso.get()
                 .load(contact.getImageUrl())
                 .into(photoImageView);
